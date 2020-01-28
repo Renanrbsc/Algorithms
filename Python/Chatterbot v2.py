dd8810f4abb633e3bb3ea71539b8275b871da11f
Renan Berti PC <renanrbsc@gmail.com>
2020-01-28 00:54:21 -0300

    Chatbot v2 com biblioteca em ingles

---
+++ Python/Chatterbot v2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#----- importing libraries -----#
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+#----- Instantiating Chatbot named Lire -----#
+bot = ChatBot('Lire')
+
+#----- Instantiating training to the bot -----#
+trainer = ChatterBotCorpusTrainer(bot)
+
+#----- setting conversation to the bot -----#
+trainer.train(
+    "chatterbot.corpus.english"
+)
+
+#----- progress looping -----#
+while True:
+    #----- Defining condition based on degree of confidence -----#
+    try:
+        response = bot.get_response(input("Você: "))
+        if float(response.confidence) > 0.1:
+            print("Lire: ", response)
+        else:
+            print("Eu não entendi :(")
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
+    
