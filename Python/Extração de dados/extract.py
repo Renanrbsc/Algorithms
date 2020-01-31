a0557e384335e55f947e4e73c162d501cafc1819
Renan Berti Ribas <renanrbsc@gmail.com>
2020-01-31 20:03:05 -0300

    obtendo dados de tabela das linguagens frequentes nos anos 2019 e 2020

--- Python/Extração de dados/extract.py
+++ Python/Extração de dados/extract.py
@@ -1,29 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
-import  matplotlib.pyplot as plt
 
 '''
-A biblioteca BeatifulSoup permite a consrução de uma árvore a partir de vários elementos de uma página HTML
-e fornece uma simples interface para acessar estes elementos.
-A biblioteca requests é para realizar pedidos HTTP. 
-A biblioteca matplotlib é para a construção de gráficos 2D para visualizar melhor os nossos dados.
+# A biblioteca BeatifulSoup permite a construção de uma árvore
+a partir de vários elementos de uma página HTML e fornece
+uma simples interface para acessar estes elementos.
+# A biblioteca requests é para realizar pedidos HTTP.
 '''
 
+# Requisitando página HTML
+pag_html = requests.get("https://www.tiobe.com/tiobe-index/").text
 
-# Requisitando página HTML (Frota ano de 2003 - Blumenau SC -- DETRAN/SC)
-'''
-A requisiçao desta URL retorna uma página HTML com os dados em tabelas, linhas e colunas referentes a quantidade
-de frotas de veículos na cidade de Blumenau ano de 2003.
-** Neste programa o dado de interesse é a quantidade de AUTOMOVEIS 
-'''
-pagina_html = requests.get("https://www.facebook.com/search/people/?q=Amanda&epa=SERP_TAB").text
+# 
+soup = BeautifulSoup(pag_html, 'html.parser')
+# print(soup.prettify())
 
-# soup = BeautifulSoup(pagina_html, 'html5lib')
-soup = BeautifulSoup(pagina_html, 'html.parser')
-#print(soup.prettify())
+# ------- Obtendo os valores de linhas e colunas --------
 
-# ------- Tratamento dos dados e obtendo os valores --------
-links = soup.find_all('a')
+# for rows in soup.find_all('tr'):
+for column in soup.find_all('td'):
+    # print(column)
+    print(column.prettify())
+    # for data in soup.get('td'):
+    #     print(data)
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
