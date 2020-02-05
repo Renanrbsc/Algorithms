bdba6195e6f239e1fb2635ad9f0879b1559b8df2
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-04 21:19:27 -0300

    corrigido exceçoes

--- Python/Extração de dados/Extracao dados pokemon.py
+++ Python/Extração de dados/Extracao dados pokemon.py
@@ -7,7 +7,7 @@ import requests
 # -- Script criado para obter dados dos pokemons e montar um banco de dados apartir do mesmo
 
 # -- Request recebe a URL da pagina
-pag_html = requests.get("https://www.pokemon.com/br/pokedex/001").text
+pag_html = requests.get(f"https://www.pokemon.com/br/pokedex/001").text
 
 # -- BeuatifulSoap estrai o html da pagina
 soup = BeautifulSoup(pag_html, 'html.parser')
@@ -32,20 +32,32 @@ def attribute():
     list_attribute = []
     
     attribute = soup.find_all("span", "attribute-value")
-    
+    # -- Altura
     h = str(attribute[0]).strip(' m</span class="attribute-value">')
-    h = float(h)
-    list_attribute.append(h)
-    
+    list_attribute.append(h) 
+    # -- Peso 
     h = str(attribute[1]).strip(' kg</span class="attribute-value">')
-    h = float(h)
     list_attribute.append(h)
-    
-    h = str(attribute[3]).strip('</span class="attribute-value">')
+    # -- Categoria
+    h = str(attribute[3]).replace('<span class="attribute-value">','')
+    h = h.replace('</span>','')
     list_attribute.append(h)
-    
-    h = str(attribute[4]).strip('</span class="attribute-value">')
-    list_attribute.append(h)
+    # -- Habilidade Principal
+    if len(attribute) >= 5:
+        h = str(attribute[4]).replace('<span class="attribute-value">','')
+        h = h.replace('</span>','')
+        list_attribute.append(h)
+    else:
+        h = ''
+        list_attribute.append(h)
+    # -- Habilidade Secundaria
+    if len(attribute) == 6:
+        h = str(attribute[5]).replace('<span class="attribute-value">','')
+        h = h.replace('</span>','')
+        list_attribute.append(h)
+    else:
+        h = ''
+        list_attribute.append(h)   
     return list_attribute
 
 def weakness():
@@ -53,14 +65,17 @@ def weakness():
     
     weakness = str(soup.find_all("div", "dtm-weaknesses"))
     a = str(weakness).split('<span>')
-    
+    # -- Fraqueza
     h = str(a[1]).split('\n\t')
     h = h[0]
     list_weakness.append(h)
-    
+    # -- Fraqueza 2
     if len(a) >= 3:
         t = str(a[2]).split('\n\t')
         t = t[0]
+        list_weakness.append(t)
+    else:
+        t = ''
         list_weakness.append(t)
     return list_weakness
 
@@ -83,10 +98,9 @@ for we in weakness():
     dados_pokemon.append(we)
     
 dados_pokemon.append(description())
-j = 1
+
 # -- Exibindo na tela valores obtidos
-    print(f'-----Pokemon Code: {j}-----')
-    for i in dados_pokemon:
-        print(i)
-    print(f'----------------------------')
-    
+print(f'-----Pokemon Code: 1-----')
+for i in dados_pokemon:
+    print(i)
+print(f'----------------------------')
