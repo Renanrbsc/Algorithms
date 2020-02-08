ec0fb04c2e8240ceff0ef1d4a510e4776c2e3988
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-08 15:27:39 -0300

    Organizando class

--- Python/Estrutura de dados/Fila_valores.py
+++ Python/Estrutura de dados/Fila_valores.py
@@ -19,21 +19,22 @@ class Fila:
         return f"      <---<---<---<---<---<---<---<---<---<---\n"
 
 
-def main():
-    fila = Fila()
+    def main(self):
+        fila = Fila()
 
-    for i in range(0, 10):
-        fila.adicionar_valor_final_da_fila(random.randint(10, 99))
-    print(f"---Fila Inicial---")
-    fila.imprimir_na_tela()
+        for i in range(0, 10):
+            self.adicionar_valor_final_da_fila(random.randint(10, 99))
+        print(f"---Fila Inicial---")
+        self.imprimir_na_tela()
 
-    for i in range(1, 11):
-        print(f"---Fila {i}---")
-        fila.retirar_primeiro_valor_da_fila()
-        fila.adicionar_valor_final_da_fila(random.randint(10, 99))
-        fila.imprimir_na_tela()
-        fila.direcao_da_fila()
+        for i in range(1, 11):
+            print(f"---Fila {i}---")
+            self.retirar_primeiro_valor_da_fila()
+            self.adicionar_valor_final_da_fila(random.randint(10, 99))
+            self.imprimir_na_tela()
+            self.direcao_da_fila()
 
 
 if __name__ == "__main__":
-    main()
+    fila = Fila()
+    fila.main()
