d745b91932c4b4cad61625d3f883f4139090d3be
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-08 13:27:48 -0300

    reproduzindo funcoes em python

--- Python/Estrutura de dados/Conversor_binario.py
+++ Python/Estrutura de dados/Conversor_binario.py
@@ -5,7 +5,7 @@ def decimal_para_binario(numero):
         decimal = decimal + str(numero % 2)
         numero = int(numero / 2)
 
-    return b[::-1]
+    return decimal[::-1]
 
 
 numero = int(input("informe um numero: "))
