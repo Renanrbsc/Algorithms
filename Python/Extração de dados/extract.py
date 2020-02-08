46937d91f7f36463b6fa96d5998fecdf4a4d4a5d
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-08 16:34:37 -0300

    organizado em class codigo de ranking de linguagens 2020, 2019

--- Python/Extração de dados/extract.py
+++ Python/Extração de dados/extract.py
@@ -8,45 +8,74 @@ uma simples interface para acessar estes elementos.
 # A biblioteca requests é para realizar pedidos HTTP.
 '''
 
-# Requisitando página HTML
-pag_html = requests.get("https://www.tiobe.com/tiobe-index/").text
 
-#
-soup = BeautifulSoup(pag_html, 'html.parser')
-#print(soup.prettify())
+class RatingLanguage:
+    """Este algoritmo tem como intuito trazer uma lista com os dados em um ranking
+    entre as linguagens mais utilizadas no ano passado e inicio de 2020"""
 
-# ------- Obtendo os valores de linhas e colunas --------
+    def __init__(self):
+        self.list_new = []
 
-data_list = []
-for column in soup.find_all('td'): # Busca as colunas da tabela html
-    string = str(column) # converte o indice de formato 'bs4' em string
-    data = string.strip('</td>') # retira caracters da string
-    data_list.append(data) # adiciona o dado em uma lista
+    def request(self):
+        # Requisitando página HTML
+        pag_html = requests.get("https://www.tiobe.com/tiobe-index/").text
 
-#print(data_list)
+        soup = BeautifulSoup(pag_html, 'html.parser')
 
-cont=0
-list_a = [] 
-list_b = []       
-for i in data_list:
-    if cont == 6:
+        return soup
+        # return soup.prettify()
+
+    def scrapper_column_rows(self):
+        """Nesta def será feito o tratamento
+         de dados dos quais foram obtidos,
+         teremos as colunas e linhas de uma tabela html"""
+
+        soup = self.request()
+
+        data_list = []
+        for column in soup.find_all('td'):
+            string = str(column)
+            data = string.strip('</td>')
+            data_list.append(data)
+
+        return data_list
+
+    def organizing_data(self):
+        """Dados da primeira lista estao fora de ordem,
+        criamos uma nova lista organizando os dados em indices de listas"""
+
+        data_list = self.scrapper_column_rows()
+
         cont = 0
-        list_b.append(list_a)
         list_a = []
-    list_a.append(i)
-    cont += 1
+        for i in data_list:
+            if cont == 6:
+                cont = 0
+                self.list_new.append(list_a)
+                list_a = []
+            list_a.append(i)
+            cont += 1
+        return self.list_new
 
-cont=0
-for i in list_b:
-    if cont == 20:
-        break
-    print(f'''Jan 2020: {i[0]}    
-Jan 2019: {i[1]}    
-Change: {i[2]}    
-Programming: {i[3]}    
-Language: {i[4]}    
-Ratings Change: {i[5]}
-''')
-    cont+=1
-    
+    def main(self):
+        """Impressao de dados obtidos conforme indice da lista de 0 até 20
+        Obtemos ranking de linguagens top20"""
 
+        self.organizing_data()
+
+        cont = 0
+        for i in self.list_new:
+            if cont == 20:
+                break
+            print(f"Jan 2020: {i[0]}\n"
+                  f"Jan 2019: {i[1]}\n"
+                  f"Change: {i[2]}\n"
+                  f"Programming: {i[3]}\n"
+                  f"Language: {i[4]}\n"
+                  f"Ratings Change: {i[5]}\n")
+            cont += 1
+
+
+if __name__ == "__main__":
+    ranking_language = RatingLanguage()
+    ranking_language.main()
